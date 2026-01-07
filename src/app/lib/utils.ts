180c92721d0d48513458d9aa1175d928dc150c57
Neel Patel <neeldpatel30@gmail.com>
2026-01-06 21:11:24 -0500

    refactor utils.ts slightly

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -48,20 +48,14 @@ export const formatDate = (format: DateFormat, dateString: string): string => {
  * Uses local time.
  * @returns The current date string.
  */
-export const todayString = (): string => {
-    const now = new Date()
-    const year = now.getFullYear()
-    const month = String(now.getMonth() + 1).padStart(2, '0')
-    const day = String(now.getDate()).padStart(2, '0')
-    return `${year}-${month}-${day}`
-}
+export const todayString = (): string => dateToLocalISOString()
 
 /**
  * Converts a Date object to a 'YYYY-MM-DD' string using local time.
- * @param date - The Date object to convert.
+ * @param date - The Date object to convert. Defaults to current date.
  * @returns The date string in 'YYYY-MM-DD' format.
  */
-export const dateToLocalISOString = (date: Date): string => {
+export const dateToLocalISOString = (date: Date = new Date()): string => {
     const year = date.getFullYear()
     const month = String(date.getMonth() + 1).padStart(2, '0')
     const day = String(date.getDate()).padStart(2, '0')
