44b676d32010c84c196baef9b0998516c6f44775
Neel Patel <neeldpatel30@gmail.com>
2026-01-16 16:31:25 -0500

    add plugin to allow svg assets to be imported in React

--- packages/landing/vite.config.ts
+++ packages/landing/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
 export default defineConfig({
@@ -16,5 +17,5 @@ export default defineConfig({
             '@shared': path.resolve(__dirname, '../shared')
         }
     },
-    plugins: [react()],
+    plugins: [react(), svgr()],
 })
