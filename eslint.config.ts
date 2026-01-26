568b8c787778bc281db825105415b1a6ed0731b1
Neel Patel <neeldpatel30@gmail.com>
2026-01-25 20:23:05 -0500

    remove unused installed devDependencies and update eslint config for typescript

---
+++ eslint.config.ts
@@ -0,0 +1,31 @@
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+	globalIgnores(['dist']),
+
+	...tseslint.configs.recommended,
+
+	{
+		files: ['packages/**/*.{ts,tsx}'],
+		extends: [
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			globals: globals.browser,
+		},
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-namespace': 'off',
+			'react-hooks/set-state-in-effect': 'off',
+			'react-hooks/incompatible-library': 'off',
+			'react-refresh/only-export-components': 'off',
+		},
+	},
+])
