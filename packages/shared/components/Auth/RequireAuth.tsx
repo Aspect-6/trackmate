00d1b14043d6e0bd38d079c66d2c82230699a90e
Neel Patel <neeldpatel30@gmail.com>
2026-01-29 19:27:14 -0500

    fix visual bug where loading spinner wasnt showing while RequireAuth component loaded

--- packages/shared/components/Auth/RequireAuth.tsx
+++ packages/shared/components/Auth/RequireAuth.tsx
@@ -22,8 +22,8 @@ const RequireAuth: React.FC<RequireAuthProps> = ({
 
     if (loading) {
         return (
-            <div className="flex h-screen w-screen items-center justify-center">
-                <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+            <div className="flex flex-1 items-center justify-center">
+                <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-500"></div>
             </div>
         )
     }
