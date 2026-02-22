b61dfbc4f5e4e6ee3dc509e544be2da8408b0307
Neel Patel <neeldpatel30@gmail.com>
2026-02-22 12:20:51 -0500

    add small margin to the left of the due date in the AssignmentCardMobileFooter on the Dashboard

--- packages/academic/pages/Dashboard/components/UpcomingAssignments/AssignmentCard/AssignmentCardMobileFooter.tsx
+++ packages/academic/pages/Dashboard/components/UpcomingAssignments/AssignmentCard/AssignmentCardMobileFooter.tsx
@@ -10,7 +10,7 @@ const AssignmentCardMobileFooter: React.FC<UpcomingAssignments.AssignmentCard.Mo
 
     return (
         <div className="flex items-center justify-between sm:hidden gap-2">
-            <div className="flex items-center gap-1 text-xs" style={{ color: DASHBOARD.TEXT_SECONDARY }}>
+            <div className="flex items-center gap-1 text-xs ml-1" style={{ color: DASHBOARD.TEXT_SECONDARY }}>
                 <Clock className="w-3 h-3" />
                 {formatDateRelative("short", assignment.dueDate)}{showTime && ` at ${formatTime(assignment.dueTime)}`}
             </div>
