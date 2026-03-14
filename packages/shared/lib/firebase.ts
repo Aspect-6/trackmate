f4aca38904b7574aba370e93c1bd69a455019634
Neel Patel <neeldpatel30@gmail.com>
2026-03-14 17:39:04 -0400

    add firebase app check

--- packages/shared/lib/firebase.ts
+++ packages/shared/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check"
 
 export const app = initializeApp({
     apiKey: "AIzaSyBJOCTTREW16d0xFHrCXUCDfqIcBCWYgv4",
@@ -10,6 +11,17 @@ export const app = initializeApp({
     messagingSenderId: "308955083111",
     appId: "1:308955083111:web:0acdc34bd99ec7a2e57fc2",
     measurementId: "G-MG9C08136S"
+})
+
+// Enable App Check debug mode on localhost
+if (window.location.hostname === "localhost") {
+    // @ts-ignore
+    self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
+
+export const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaEnterpriseProvider("6LfiPIosAAAAAO0suQC9IeLUZdczvM8qs6B2mpYA"),
+    isTokenAutoRefreshEnabled: true
 })
 
 export const auth = getAuth(app)
